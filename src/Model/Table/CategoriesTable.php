d146efc5dfd667e97c62d501a46566f7c9d60712
Maicon Pinto <maiconsilva.pinto@gmail.com>
2016-04-14 17:46:42 -0300

    Generate Bake All Categories

---
+++ src/Model/Table/CategoriesTable.php
@@ -0,0 +1,95 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Category;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Categories Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $ParentCategories
+ * @property \Cake\ORM\Association\HasMany $Articles
+ * @property \Cake\ORM\Association\HasMany $ChildCategories
+ */
+class CategoriesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('categories');
+        $this->displayField('name');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+        $this->addBehavior('Tree');
+
+        $this->belongsTo('ParentCategories', [
+            'className' => 'Categories',
+            'foreignKey' => 'parent_id'
+        ]);
+        $this->hasMany('Articles', [
+            'foreignKey' => 'category_id'
+        ]);
+        $this->hasMany('ChildCategories', [
+            'className' => 'Categories',
+            'foreignKey' => 'parent_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->integer('lft')
+            ->requirePresence('lft', 'create')
+            ->notEmpty('lft');
+
+        $validator
+            ->integer('rght')
+            ->requirePresence('rght', 'create')
+            ->notEmpty('rght');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->requirePresence('description', 'create')
+            ->notEmpty('description');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['parent_id'], 'ParentCategories'));
+        return $rules;
+    }
+}
